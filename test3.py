d74fdb82dedac1b256ae170d13664e0316dda26e
techprasanna <prasanna.bhope@gmail.com>
2022-03-28 20:23:00 -0400

    Updated Code. Updated IP

--- test3.py
+++ test3.py
@@ -6,7 +6,7 @@ import pickle
 import struct ### new code
 cap=cv2.VideoCapture('/Users/prasannasmac/Documents/Capstone/testvideo.mp4')
 clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(('localhost',8089))
+clientsocket.connect(('192.168.200.55',8089))
 while True:
     ret,frame=cap.read()
     data = pickle.dumps(frame)
